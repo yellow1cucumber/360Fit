e07c5434cff376a1b2f63ea52fb11c3926ea20ae
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-13 10:57:34 +0500

    FEAT: create models for: - Product - Nomenclature - Storage - CashRegister - Supplier

---
+++ Domain.Core/Products/Product.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Core.Sells;
+
+namespace Domain.Core.Products
+{
+    public class Product : SalableObject
+    {
+        public string BarCode { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Domain.Core/Products/Storage.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Core.Products
+{
+    public class Storage : Model
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public IEnumerable<StoragedProduct> Products { get; set; }
+    }
+}
