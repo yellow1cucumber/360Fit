8bdf1a4180e2433c6ff1794bbfc52463dc874a64
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-13 17:56:37 +0500

    FEAT: add UsersMutations

---
+++ Core.Domain/GraphQl/Mutations/UsersMutation.cs
@@ -0,0 +1,35 @@
+﻿using DAL;
+using Domain.Core.Users;
+
+namespace API.Gate.GraphQl.Mutations
+{
+    public class UsersMutation
+    {
+        [UseProjection]
+        [UseFiltering]
+        public async Task<IResult> CreateOrUpdateUser([Service]Context context, User user)
+        {
+            if (context.Users.Any(x => x.Id == user.Id)){
+                context.Users.Update(user);
+                await context.SaveChangesAsync();
+                return Results.Ok(user);
+            }
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+            return Results.Created(uri: String.Empty, value: user);
+        }
+
+
+        [UseFiltering]
+        public async Task<IResult> RemoveUser([Service] Context context, User user)
+        {
+            if (context.Users.Contains(user))
+            {
+                context.Users.Remove(user);
+                await context.SaveChangesAsync();
+                return Results.Ok();
+            }
+            return Results.NotFound(user);
+        }
+    }
+}
