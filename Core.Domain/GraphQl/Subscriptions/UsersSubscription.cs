b6e653e9b53b282506cf224243f24430205d4470
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-23 12:18:36 +0500

    FEAT: define users subscriptions class

---
+++ Core.Domain/GraphQl/Subscriptions/UsersSubscription.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Users;
+
+namespace API.Gate.GraphQl.Subscriptions
+{
+    [ExtendObjectType("Subscription")]
+    public class UsersSubscription
+    {
+        [Subscribe]
+        public User OnUserCreated([EventMessage] User user)
+            => user;
+
+        [Subscribe]
+        public User OnUserChanged([EventMessage] User user)
+            => user;
+    }
+}
