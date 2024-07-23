b9b68b4191eba9ce300133e0ae3785e18bff42e3
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-23 12:16:35 +0500

    FEAT: define services subscription class

---
+++ Core.Domain/GraphQl/Subscriptions/ServiceSubscription.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Sells.Service;
+
+namespace API.Gate.GraphQl.Subscriptions
+{
+    [ExtendObjectType("Subscription")]
+    public class ServiceSubscription
+    {
+        [Subscribe]
+        public Service OnServiceCreated([EventMessage] Service service)
+            => service;
+
+        [Subscribe]
+        public Service OnServiceChanged([EventMessage] Service service)
+            => service;
+    }
+}
