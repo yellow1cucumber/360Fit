14bccc3572d7fbe68c05f3e700dcc1abc6f72722
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-23 12:08:16 +0500

    FEAT: define product subscriptions class

---
+++ Core.Domain/GraphQl/Subscriptions/ProductsSubscription.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Sells.Products;
+
+namespace API.Gate.GraphQl.Subscriptions
+{
+    [ExtendObjectType("Subscription")]
+    public class ProductsSubscription
+    {
+        [Subscribe]
+        public Product OnProductCreated([EventMessage] Product product)
+            => product;
+
+        [Subscribe]
+        public Product OnProductChanged([EventMessage] Product product)
+            => product;
+    }
+}
