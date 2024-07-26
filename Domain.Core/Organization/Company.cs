a03d7da097807ecadd321064d813513fd20073b7
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-26 19:36:22 +0500

    FEAT: add Company class

---
+++ Domain.Core/Organization/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Core.Organization
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        public string PublicName { get; set; }
+        public string Description {  get; set; }
+
+        public Requisites Requisites { get; set; }
+    }
+}
