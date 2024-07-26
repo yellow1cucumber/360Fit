94595ce891992499c61b0e02321760b1792cd5a5
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-26 19:44:37 +0500

    FEAT: add instagram, website url, linkedin accout to contacts

--- Domain.Core/Organization/Contact/Contacts.cs
+++ Domain.Core/Organization/Contact/Contacts.cs
@@ -4,5 +4,9 @@
     {
         public IEnumerable<PhoneNumber> Phones { get; set; }
         public IEnumerable<string> Emails { get; set; }
+
+        public string Instagram { get; set; }
+        public string WebSite { get; set; }
+        public string LinkedIn { get; set; }
     }
 }
