99cecfcecae0685f2254842999443bf2df7448e9
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-26 20:48:43 +0500

    FEAT: add company categories

--- Domain.Core/Organization/Company.cs
+++ Domain.Core/Organization/Company.cs
@@ -7,7 +7,17 @@ namespace Domain.Core.Organization
         public string PublicName { get; set; }
         public string Description {  get; set; }
 
+        public CompanyCategory Category { get; set; }
+
         public Requisites Requisites { get; set; }
         public Contacts Contacts { get; set; }
+
+        public enum CompanyCategory
+        {
+            Developer = 0,
+            FitnessCentre = 1,
+            SportsNutritionStore = 2,
+            SportsNutritionSupplier = 3
+        }
     }
 }
