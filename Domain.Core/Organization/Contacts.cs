458aa84198948d599d2611834a816904c4539eb2
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-26 19:37:52 +0500

    FEAT: add contacts

---
+++ Domain.Core/Organization/Contacts.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Core.Organization
+{
+    public class Contacts : Model
+    {
+        public IEnumerable<string> Phones { get; set; }
+        public IEnumerable<string> Emails { get; set; }
+    }
+}
