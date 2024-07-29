9f3aa7a0c251078f2e230991b523eabda20e1d3c
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-29 12:23:34 +0500

    FEAT: add identification service

---
+++ API.Auth/Services/IdentificationService.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using Domain.Core.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Auth.Services
+{
+    public class IdentificationService
+    {
+        private readonly Context context;
+        private readonly Repository<User> repository;
+
+        public IdentificationService(Context context)
+        {
+            this.context = context;
+            this.repository = new Repository<User>(context);
+        }
+
+        public async Task<User?> IdentificateUser(string phoneNumber)
+        {
+            User? user = await this.repository.GetAll()
+                                              .FirstOrDefaultAsync(user => user.PhoneNumber == phoneNumber);
+            if (user is null)
+            {
+                throw new Exception("User not identificated");
+            }
+
+            return user;
+        }
+    }
+}
