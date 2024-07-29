f97008f2351b6ec2bb7ea070f655e89e4861e680
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-07-29 12:42:19 +0500

    FEAT: add userNotIdentificatedException

---
+++ API.Auth/Exceptions/UserNotIdentificatedException.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Auth.Exceptions
+{
+    public class UserNotIdentificatedException : Exception
+    {
+        public UserNotIdentificatedException(Exception? innerException, string phone)
+            : base(UserNotIdentificatedException.CompileMessage(phone),
+                  innerException)
+            => this.Phone = phone;
+
+        public string Phone { get; set; }
+
+        private static string CompileMessage(string phonenumber) {
+            return $"ERROR: user with phone number: {phonenumber} not found. Identification was unsuccessfull";
+        }
+    }
+}
