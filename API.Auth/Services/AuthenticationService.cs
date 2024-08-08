4ace3c9170a87ca6387d0b98b19252cccbec0646
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-08-08 22:22:50 +0500

    FEAT: add authentification service

---
+++ API.Auth/Services/AuthenticationService.cs
@@ -0,0 +1,22 @@
+﻿using DAL;
+using Domain.Core.Users;
+
+namespace API.Auth.Services
+{
+    public class AuthenticationService
+    {
+        private readonly Context context;
+        private readonly Repository<User> repository;
+
+        public AuthenticationService(Context context)
+        {
+            this.context = context;
+            this.repository = new Repository<User>(context);
+        }
+
+        public async Task<bool> AuthentificateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
