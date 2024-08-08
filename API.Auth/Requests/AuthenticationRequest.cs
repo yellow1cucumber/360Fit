398d2d8a100ebc0973ed46aeae542ceac85d19a5
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-08-08 23:59:03 +0500

    FEAT: add authentication request

---
+++ API.Auth/Requests/AuthenticationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Auth.Requests
+{
+    public class AuthenticationRequest
+    {
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
