26b3bb8379980041448ab3e0365dbf45de921cb8
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-22 15:13:42 +0500

    FEAT: add organization mapping profile

---
+++ Infrastructure.DTO/Profiles/OrganizationProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain.Core.Organization;
+using Domain.Core.Organization.Contact;
+
+namespace Infrastructure.DTO.Profiles
+{
+    public class OrganizationProfile : Profile
+    {
+        public OrganizationProfile()
+        {
+            this.CreateMap<Contacts, ContactsDTO>();
+            this.CreateMap<PhoneNumber, PhoneNumberDTO>();
+            this.CreateMap<BankRequisites, BankRequisitesDTO>();
+            this.CreateMap<Company, CompanyDTO>();
+            this.CreateMap<Requisites, RequisitesDTO>();
+        }
+    }
+}
