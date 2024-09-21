0208b7246074de169efc26c9ca510910e3148127
yellow1cucumber <sadovskiy1n@gmail.com>
2024-09-21 17:01:57 +0500

    FEAT: add attribute for db stored models for auto repository gen

---
+++ API.Gate/Configuration/DALExtension.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+using DAL;
+
+using SlnAssembly.Attributes;
+
+namespace API.Gate.Configuration
+{
+    public static class DALExtension
+    {
+        public static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var dbStoredModels = GetDbStoredTypes(assembly, typeof(DALRepository));
+            foreach ( var type in dbStoredModels)
+            {
+                services.AddRepository(type);
+            }
+            return services;
+        }
+
+        private static IServiceCollection AddRepository(this IServiceCollection services, Type modelType)
+        {
+            var repository = typeof(Repository<>).MakeGenericType(modelType);
+            services.AddTransient(repository);
+
+            return services;
+        }
+
+        private static IEnumerable<Type> GetDbStoredTypes(Assembly assembly, Type attributeType)
+        {
+            foreach(var type in assembly.GetTypes())
+            {
+                if(type.GetCustomAttributes(attributeType, true).Length > 0)
+                {
+                    yield return type;
+                }
+            }
+        }
+    }
+}
